911d1e3a549c2d36bd2b1dcb0def8e7fa21cefb2
huangyuanhui <2278181701@qq.com>
2022-06-28 23:02:55 +0800

    一人一单问题优化：先获取锁，再提交事务，最后再释放锁（把整个函数锁起来，即事务提交之后，再去释放锁！）

--- src/main/java/com/hmdp/service/impl/VoucherOrderServiceImpl.java
+++ src/main/java/com/hmdp/service/impl/VoucherOrderServiceImpl.java
@@ -45,45 +45,42 @@ public class VoucherOrderServiceImpl extends ServiceImpl<VoucherOrderMapper, Vou
             return Result.fail("库存不足！");
         }
         Long userId = UserHolder.getUser().getId();
-        // 创建订单
-        return createVoucherOrder(voucherId);
+        // 先获取锁，再提交事务，最后再释放锁
+        synchronized (userId.toString().intern()) {
+            return createVoucherOrder(voucherId);
+        }
     }
 
     /**
-     * 此方法是有问题的，因为是在方法内部加的锁，而事务是在方法上，那么此时，会先释放锁，再去提交事务。
-     * 那么会有这种情况：锁释放了，意味着其他线程可以进来，但是事务尚未提交，即新增的订单很有可能还没写到数据库，
-     * 有新线程进来查询订单会依然查不到，那么依然会有一人一单问题
-     * 因此：这个方法的问题是锁的范围有点小，应该把整个函数锁起来，即事务提交之后，再去释放锁
+     * 创建订单
+     * @param voucherId
+     * @return
      */
     @Transactional
     public Result createVoucherOrder(Long voucherId) {
         // 一人一单：在扣减库存前，判断用户是否下过单了
         Long userId = UserHolder.getUser().getId();
-        // 所谓一人一单，就是同一个用户来了，才去判断并发安全问题，也就是说只需要对用户加锁，也就是说同一个用户加同一把锁，缩小锁的范围，提高性能
-        // 注意：不能使用userId.toString()，因为该方法实现里是一个全新的对象、全新的字符串；应该使用：userId.toString().intern()
-        synchronized (userId.toString().intern()) {
-            int count = query().eq("voucher_id", voucherId).eq("user_id", userId).count();
-            if (count > 0) {
-                return Result.fail("该用户已经购买过了！");
-            }
-
-            // 扣减库存
-            boolean success = seckillVoucherService.update()
-                    .setSql("stock = stock - 1")    // set stock = stock - 1
-                    .eq("voucher_id", voucherId).gt("stock", 0) // voucher_id = ? and stock > 0
-                    .update();
-            if (!success) {
-                return Result.fail("库存不足！");
-            }
-
-            // 创建订单
-            VoucherOrder voucherOrder = new VoucherOrder();
-            long orderId = redisIdWorker.nextId("order");
-            voucherOrder.setId(orderId);
-            voucherOrder.setVoucherId(voucherId);
-            voucherOrder.setUserId(userId);
-            save(voucherOrder);
-            return Result.ok(orderId);
+        int count = query().eq("voucher_id", voucherId).eq("user_id", userId).count();
+        if (count > 0) {
+            return Result.fail("该用户已经购买过了！");
         }
+
+        // 扣减库存
+        boolean success = seckillVoucherService.update()
+                .setSql("stock = stock - 1")    // set stock = stock - 1
+                .eq("voucher_id", voucherId).gt("stock", 0) // voucher_id = ? and stock > 0
+                .update();
+        if (!success) {
+            return Result.fail("库存不足！");
+        }
+
+        // 创建订单
+        VoucherOrder voucherOrder = new VoucherOrder();
+        long orderId = redisIdWorker.nextId("order");
+        voucherOrder.setId(orderId);
+        voucherOrder.setVoucherId(voucherId);
+        voucherOrder.setUserId(userId);
+        save(voucherOrder);
+        return Result.ok(orderId);
     }
 }

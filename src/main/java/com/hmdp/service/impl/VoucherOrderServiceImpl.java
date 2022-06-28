9b8609c6cb0d859edafe6e0b908dccfa38935a33
huangyuanhui <2278181701@qq.com>
2022-06-28 22:58:58 +0800

    一人一单问题优化： 问题说明： 此方法是有问题的，因为是在方法内部加的锁，而事务是在方法上，那么此时，会先释放锁，再去提交事务。那么会有这种情况：锁释放了，意味着其他线程可以进来，但是事务尚未提交，即新增的订单很有可能还没写到数据库，有新线程进来查询订单会依然查不到，那么依然会有一人一单问题。 因此：这个方法的问题是锁的范围有点小，应该把整个函数锁起来，即事务提交之后，再去释放锁！

--- src/main/java/com/hmdp/service/impl/VoucherOrderServiceImpl.java
+++ src/main/java/com/hmdp/service/impl/VoucherOrderServiceImpl.java
@@ -44,14 +44,16 @@ public class VoucherOrderServiceImpl extends ServiceImpl<VoucherOrderMapper, Vou
         if (seckillVoucher.getStock() < 1) {
             return Result.fail("库存不足！");
         }
+        Long userId = UserHolder.getUser().getId();
         // 创建订单
         return createVoucherOrder(voucherId);
     }
 
     /**
-     * 创建订单
-     * @param voucherId
-     * @return
+     * 此方法是有问题的，因为是在方法内部加的锁，而事务是在方法上，那么此时，会先释放锁，再去提交事务。
+     * 那么会有这种情况：锁释放了，意味着其他线程可以进来，但是事务尚未提交，即新增的订单很有可能还没写到数据库，
+     * 有新线程进来查询订单会依然查不到，那么依然会有一人一单问题
+     * 因此：这个方法的问题是锁的范围有点小，应该把整个函数锁起来，即事务提交之后，再去释放锁
      */
     @Transactional
     public Result createVoucherOrder(Long voucherId) {

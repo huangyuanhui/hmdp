af68e9974264040f12412d257095ae4bbd7dcb4c
huangyuanhui <2278181701@qq.com>
2022-06-29 23:04:54 +0800

    redis实现分布式锁版本二 版本一在极端情况下，是有问题的，解决方法：在释放锁的时候，判断一下锁的标识是否与当前线程获取锁的时候一样。

--- src/main/java/com/hmdp/utils/SimpleRedisLock.java
+++ src/main/java/com/hmdp/utils/SimpleRedisLock.java
@@ -1,5 +1,6 @@
 package com.hmdp.utils;
 
+import cn.hutool.core.lang.UUID;
 import org.springframework.data.redis.core.StringRedisTemplate;
 
 import java.util.concurrent.TimeUnit;
@@ -17,19 +18,33 @@ public class SimpleRedisLock implements ILock {
         this.redisTemplate = redisTemplate;
     }
 
+    /**
+     * 锁前缀
+     */
     private static final String KEY_PREFIX = "lock:";
+    /**
+     * 线程id前缀：UUID标识jvm
+     */
+    private static final String ID_PREFIX = UUID.randomUUID().toString(true) + "-";
 
     @Override
     public boolean tryLock(long expireSec) {
-        // 获取线程标示
-        long threadId = Thread.currentThread().getId();
-        // 获取锁
-        Boolean success = redisTemplate.opsForValue().setIfAbsent(KEY_PREFIX + name, threadId + "", expireSec, TimeUnit.SECONDS);
+        // 获取线程标示，（问题：线程id在jvm中是递增的，但是多个jvm中线程标识可能会一样的情况）
+        //long threadId = Thread.currentThread().getId();
+        //Boolean success = redisTemplate.opsForValue().setIfAbsent(KEY_PREFIX + name, threadId + "", expireSec, TimeUnit.SECONDS);
+
+        String threadId = ID_PREFIX + Thread.currentThread().getId();  // UUID标识jvm，线程id标识线程
+        Boolean success = redisTemplate.opsForValue().setIfAbsent(KEY_PREFIX + name, threadId, expireSec, TimeUnit.SECONDS);
         return Boolean.TRUE.equals(success);
     }
 
     @Override
     public void unLock() {
-        redisTemplate.delete(KEY_PREFIX + name);
+        // 获取线程标示
+        String threadId = ID_PREFIX + Thread.currentThread().getId();
+        String id = redisTemplate.opsForValue().get(KEY_PREFIX + name);
+        if (threadId.equals(id)) {
+            redisTemplate.delete(KEY_PREFIX + name);
+        }
     }
 }
